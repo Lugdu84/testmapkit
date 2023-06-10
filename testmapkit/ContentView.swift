569aacf2819825e1efb913c6df11ba8524646f31
David Grammatico <david.lugdunum@gmail.com>
2023-06-10 11:39:55 +0200

    Initial Commit

---
+++ testmapkit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testmapkit
+//
+//  Created by David Grammatico on 10/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testmapkit/testmapkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testmapkitApp.swift
+//  testmapkit
+//
+//  Created by David Grammatico on 10/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct testmapkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
